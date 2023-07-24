8066b5872baf066362121da458ee50e9b051166d
Ajay <118235813+Ajaygit0987@users.noreply.github.com>
2023-07-24 11:40:15 +0530

    Update upload_log_file.sh

--- upload_log_file.sh
+++ upload_log_file.sh
@@ -4,49 +4,61 @@
 base_directory="/root/sample/script-02/log"
 
 # Specify the Amazon S3 bucket details
-bucket_name="sample-test-0101"
+bucket_name="sample-test-0101/logs"
 
-# Specify the path to store the zip files
-output_directory="/root/sample/script-02/log"
+# Specify the AWS CLI binary path
+aws_cli="/usr/local/bin/aws"
 
 # Function to zip a file
 zip_file() {
     file_path="$1"
     file_name=$(basename "$file_path")
     zip_filename="${file_name%.*}.zip"
-    zip_path="$output_directory/$zip_filename"
+    zip_path="/tmp/$zip_filename"  # Store the zip file temporarily in /tmp directory
 
-    # Create a zip file for the file
-    zip "$zip_path" "$file_path"
+    # Create a zip file for the file (without the directory structure)
+    zip -j "$zip_path" "$file_path"
 
     # Upload the zip file to Amazon S3
-    s3_key="$zip_filename"
-    aws s3 cp "$zip_path" "s3://$bucket_name/$s3_key"
-    echo "Uploaded $zip_filename to S3 bucket $bucket_name with key: $s3_key"
+    "$aws_cli" s3 cp "$zip_path" "s3://$bucket_name/"
+    echo "Uploaded $zip_filename to S3 bucket $bucket_name"
+
+    # Remove the temporary zip file
+    rm "$zip_path"
 }
 
-# Get the list of log files sorted by modification time (oldest to newest)
-log_files=$(find "$base_directory" -type f -name "*.log" -printf "%T@ %p\n" | sort -n | cut -d' ' -f2-)
+# Check if the AWS CLI is installed at the specified path
+if [ ! -x "$aws_cli" ]; then
+    echo "AWS CLI is not installed. Please install it at $aws_cli and configure AWS credentials."
+    exit 1
+fi
 
-# Count the number of log files
-count=$(echo "$log_files" | wc -l)
+# Change to the base directory to ensure relative paths work correctly
+cd "$base_directory" || exit
 
-# Zip and upload all but the last two log files
-current_count=0
-for log_file in $log_files; do
-    if [ $current_count -lt $((count-2)) ]; then
+# Zip and upload log files, keeping the last two
+log_files=$(find . -maxdepth 1 -type f -name "*.log" -printf "%T@ %p\n" | sort -n | cut -d' ' -f2-)
+
+total_logs=$(echo "$log_files" | wc -l)
+count=0
+
+echo "$log_files" | while IFS= read -r log_file; do
+    if [ $count -lt $((total_logs - 2)) ]; then
         zip_file "$log_file"
         rm "$log_file"
-        rm "${log_file%.*}.zip"
     fi
-    current_count=$((current_count+1))
+    count=$((count + 1))
 done
 
-# Remove all log files except the last two files
-ls -t "$base_directory"/*.log 2>/dev/null | tail -n +3 | xargs rm -f
-
 # Remove all zip files except the last two files
-zip_files_count=$(ls -t "$output_directory"/*.zip 2>/dev/null | wc -l)
-if [ $zip_files_count -gt 2 ]; then
-    ls -t "$output_directory"/*.zip 2>/dev/null | tail -n +3 | xargs rm -f
-fi
+zip_files=$(find /tmp -maxdepth 1 -type f -name "*.zip" -printf "%T@ %p\n" | sort -n | cut -d' ' -f2-)
+
+total_zips=$(echo "$zip_files" | wc -l)
+count=0
+
+echo "$zip_files" | while IFS= read -r zip_file; do
+    if [ $count -lt $((total_zips - 2)) ]; then
+        rm "$zip_file"
+    fi
+    count=$((count + 1))
+done
