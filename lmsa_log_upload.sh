a75d9eafd5b65efbbe4ca847581febcbfca1ade4
Ajay <118235813+Ajaygit0987@users.noreply.github.com>
2024-07-24 16:25:12 +0530

    Update lmsa_log_upload.sh
    updated-july24

--- lmsa_log_upload.sh
+++ lmsa_log_upload.sh
@@ -6,7 +6,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S')"
 
 # Command 1: Zip log files
 echo "Started to zipping the files"
-for file in /opt/apache-tomcat-8.5.97/bin/logs/inspirenetz-api.*-*-*.log; do
+for file in /opt/apache-tomcat-8.0.22/bin/logs/inspirenetz-api.*-*-*.log; do
     zip -r "${file%.*}.zip" "$file"
 done
 echo "zipping the files is completed"
@@ -14,21 +14,21 @@ echo "zipping the files is completed"
 
 # Command 2: Upload zipped logs to AWS S3
 echo "Started uploading the files to s3"
-for zip_file in /opt/apache-tomcat-8.5.97/bin/logs/inspirenetz-api.*-*-*.zip; do
-    aws s3 cp "$zip_file" s3://adl-logs-test/adl-dev/lmsa/
+for zip_file in /opt/apache-tomcat-8.0.22/bin/logs/inspirenetz-api.*-*-*.zip; do
+    aws s3 cp "$zip_file" s3://inspz-app-logs/lmsa-logs/adl-prod-lmsa-01/
 done
 echo "completed of uploading the files to s3 "
 
 # Command 3: Remove log files 
-sudo rm /opt/apache-tomcat-8.5.97/bin/logs/inspirenetz-api.*-*-*.log
+sudo rm /opt/apache-tomcat-8.0.22/bin/logs/inspirenetz-api.*-*-*.log
 echo "original log files are removed"
 
 # Command 4: Deleting the zip files  except last 15 days
-cd /opt/apache-tomcat-8.5.97/bin/logs/
-pwd 
+cd /opt/apache-tomcat-8.0.22/bin/logs/
+pwd
 ls -1 | head -n -15 | xargs rm
-echo "zipped files are removed keeping the last 15 days file"
+echo "zipped files are removed keeping the last 20 days file"
 
 #Command 5: To print Date and Time
 echo "process is completed"
-echo "$(date '+%Y-%m-%d %H:%M:%S')" 
+echo "$(date '+%Y-%m-%d %H:%M:%S')"
